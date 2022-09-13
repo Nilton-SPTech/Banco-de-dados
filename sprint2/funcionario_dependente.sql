782a7d11b96b113637e774ff530d74c2c32386fa
Nilton-SPTech <nilton.gomes@sptech.school>
2022-09-13 14:59:34 -0300

    Trabalhando com tabelas dependentes (Relacionamento Fraco)

--- sprint2/funcionario_dependente.sql
+++ sprint2/funcionario_dependente.sql
@@ -3,20 +3,60 @@ CREATE DATABASE sprint2;
 USE sprint2; 
 
 CREATE TABLE Funcionario(
-
-    idFunc INT PRIMARY KEY AUTO_INCREMENT
-
+    idFunc INT PRIMARY KEY AUTO_INCREMENT, 
+    nome VARCHAR(45), 
+    salario DECIMAL(3,2)  --(total_caractere , qnt_depois_virgula)
 );
+
+-- ESSA É UMA ENTIDADE FRACA
+CREATE TABLE Dependente(
+    idDep INT AUTO_INCREMENT,
+    nome VARCHAR(45),
+    parentesco VARCHAR(45), 
+
+    fkFunc INT, FOREIGN KEY (fkFunc) REFERENCES Funcionario (idFunc), -- CHAVE ESTRANGEIRA
+
+    PRIMARY KEY (idDep, fkFunc) -- CHAVE PRIMÁRIA COM DOIS CAMPOS O ID E A FK, PQ ELA DEPENTE
+)AUTO_INCREMENT= 100; 
+
+
+-- INSERINDO DADOS
+INSERT INTO Funcionario VALUES 
+    (null, 'Paulo', 0.99), 
+    (null, 'Vivian', 0.99);
+
+
+INSERT INTO Dependente VALUES
+    (null, 'Paula','irmã',1),
+    (null,'Rafaela', 'irmã',2),
+    (null, 'Paulão', 'pai',1);
+
+
+SELECT  f.nome 'Funcionario', d.nome 'Dependente' FROM Funcionario as f
+    JOIN Dependente as d
+        ON f.idFunc = d.fkFunc; 
+
+SELECT CONCAT('O funcionario ', f.nome, ' tem como dependente o(a)', d.nome) as 'Pessoas que dependem do funcionario' FROM Funcionario as f
+    JOIN Dependente as d
+        ON f.idFunc = d.fkFunc; 
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--PERMISSÕES DCL (Data Control Language)
 
 -- CRIANDO O USUARIO PARA TER ACESSO
 CREATE USER 'estagiario'@'localhost' IDENTIFIED BY
     'SPTECH';
 
--- DANDO PRIVILÉGIOS PARA TODAS AS TABELAS 
+-- DANDO PRIVILÉGIOS PARA TODAS AS TABELAS  - pt2.1
 GRANT ALL privileges ON faculdade1adsc.*
     TO 'estagiario'@'localhost';
 
---DANDO ALGUNS PRIVILÉGIOS NAS TABELAS, ELE NÃO PODE FAZER TUDO
+--DANDO ALGUNS PRIVILÉGIOS NAS TABELAS, ELE NÃO PODE FAZER TUDO  - pt2.2
 GRANT SELECT,UPDATE,DELETE,CREATE ON
     faculdade1adsc.*
     TO 'estagiario'@'localhost';
@@ -24,4 +64,3 @@ GRANT SELECT,UPDATE,DELETE,CREATE ON
 -- DANDO CERTEZA PARA EMPREGAR AS PERMISSÕES
 FLUSH privileges; 
 
-DELETE USER
